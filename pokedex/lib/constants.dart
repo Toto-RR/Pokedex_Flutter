dc3cddbad4c6df02e9c46c82e355e7740121dcd5
Víctor Gil Espallargas <victorgile14@gmail.com>
2023-12-17 19:53:52 +0100

    const definitions

---
+++ pokedex/lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// COLORS ------------------------------------
+const Color redPokedex = Color(0xFFcc322a);
+const Color yellowPokedex = Color(0xFFe6d596);
+const Color darkRedPokedex = Color(0xFF642c34);
+const Color oldRedPokedex = Color(0xFFb45c64);
+const Color bluePokedex = Color(0xFFa7b9d3);
+const Color brownPokedex = Color(0xFF4c3434);
+const Color darkPokedex = Color.fromARGB(255, 29, 29, 29);
+
+
+// TEXT STYLES -------------------------------
+TextStyle whiteNormalStyle = const TextStyle(color: Colors.white, fontSize: 20);
+TextStyle whiteNormalStyleBig = const TextStyle(color: Colors.white, fontSize: 25);
+TextStyle blackNormalStyle = const TextStyle(color: Colors.black, fontSize: 20);
+TextStyle pillsStyle = TextStyle(color: redPokedex, fontSize: 20, fontWeight: FontWeight.w600);
