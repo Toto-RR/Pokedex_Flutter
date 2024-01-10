686111836ad0a8bb2d45644dba045842f23e520a
Toto-RR <antonio.romanos@estudiantat.upc.edu>
2024-01-10 02:51:19 +0100

    infoscreen updated

--- pokedex/lib/screens/info_screen.dart
+++ pokedex/lib/screens/info_screen.dart
@@ -3,7 +3,10 @@ import 'package:pokedex/api/poke_api.dart';
 import 'package:pokedex/constants.dart';
 import 'package:pokedex/information/abilities_info.dart';
 import 'package:pokedex/information/pokemon_info.dart';
+import 'package:pokedex/provider/favorite_provider.dart';
+import 'package:pokedex/screens/infoscreen/pokemon_content.dart';
 import 'package:pokedex/widget/my_appbar.dart';
+import 'package:provider/provider.dart';
 
 class InfoScreen extends StatefulWidget {
   final String pokemonName;
@@ -41,12 +44,14 @@ class _InfoScreenState extends State<InfoScreen> {
     super.initState();
     pokemonInfo = PokemonInfo.empty(
         widget.id, widget.pokemonName, widget.types, widget.stats);
-        _loadPokemonInfo();
+     _loadPokemonInfo();
   }
 
   Future<void> _loadPokemonInfo() async {
     try {
+      //Get the information (about and info tab)
       final pokemonSpecies = await ApiService.fetchPokemonInfo(widget.pokemonName);
+      //Get the information (name, id, abilities, stats)
       final pokemonData = await ApiService.fetchPokemon(widget.pokemonName);
       List<Ability> abilities = (pokemonData['abilities'] as List)
           .map((abilityJson) => Ability.fromJson(abilityJson))
@@ -73,6 +78,7 @@ class _InfoScreenState extends State<InfoScreen> {
         abilities: abilities,
       );
 
+      _loadGenerationInfo();
     } catch (e) {
       // ignore: avoid_print
       print("Error al cargar la información del Pokémon: $e");
@@ -82,14 +88,77 @@ class _InfoScreenState extends State<InfoScreen> {
     }
   }
 
+  Future<void> _loadGenerationInfo() async {
+    try {
+} catch (e) {
+      // ignore: avoid_print
+      print("Error al cargar la información de la generación: $e");
+    }
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    bool isFavorite = context.watch<FavoriteProvider>().isFavorite(pokemonInfo);
 
     return Scaffold(
       appBar: MyAppBar(appBarText: widget.pokemonName.toUpperCase()),
       backgroundColor: darkPokedex,
       body: Column(
-       
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Stack(
+            alignment: Alignment.topRight,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                child: Image.network(
+                  ApiService.getPokemonImageUrl(int.parse(widget.id)),
+                  height: 300,
+                  width: 300,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  if (isFavorite) {
+                    context
+                        .read<FavoriteProvider>()
+                        .removeFromFavorites(pokemonInfo);
+                  } else {
+                    context
+                        .read<FavoriteProvider>()
+                        .addToFavorites(pokemonInfo);
+                  }
+                },
+                icon: Icon(
+                  isFavorite ? Icons.favorite : Icons.favorite_border,
+                  color: Colors.red,
+                  size: 30,
+                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: PokemonContent(
+              id: widget.id,
+              pokemonName: widget.pokemonName,
+              types: widget.types,
+              stats: widget.stats,
+              description: pokemonInfo.description,
+              baseHappiness: pokemonInfo.baseHappiness,
+              captureRate: pokemonInfo.captureRate,
+              evolvesFrom: pokemonInfo.evolvesFrom,
+              growthRate: pokemonInfo.growthRate,
+              habitat: pokemonInfo.habitat,
+              height: pokemonInfo.height.toDouble(),
+              weight: pokemonInfo.weight.toDouble(),
+              abilities: pokemonInfo.abilities,
+            ),
+          ),
+        ],
       ),
     );
   }
