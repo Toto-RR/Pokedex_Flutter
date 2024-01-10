472e61dd9bb9e63a6cbb00ade98823570a80e911
Toto-RR <antonio.romanos@estudiantat.upc.edu>
2024-01-10 01:57:46 +0100

    initialscreen updated

--- pokedex/lib/screens/initial_screen.dart
+++ pokedex/lib/screens/initial_screen.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:pokedex/api/poke_api.dart';
+import 'package:pokedex/constants.dart';
+import 'package:pokedex/provider/pokemon_provider.dart';
+import 'package:pokedex/widget/my_appbar.dart';
+import 'package:pokedex/widget/poke_card.dart';
+import 'package:pokedex/widget/toolbar.dart';
+import 'package:provider/provider.dart';
+
 class InitialScreen extends StatefulWidget {
   const InitialScreen({Key? key}) : super(key: key);
 
@@ -9,43 +15,77 @@ class InitialScreen extends StatefulWidget {
 
 
 class _InitialScreenState extends State<InitialScreen> {
-  late Future<List<Map<String, dynamic>>> futurePokemonList;
   late ScrollController _scrollController;
-  late List<Map<String, dynamic>> pokemonList;
-  int offset = 0;
-  int limit = 50;
+  late PokemonListProvider _pokemonListProvider;
 
   @override
   void initState() {
     super.initState();
     _scrollController = ScrollController();
-    pokemonList = [];
-    futurePokemonList = _fetchPokemonList(offset, limit);
+    _pokemonListProvider = PokemonListProvider();
+    _pokemonListProvider.fetchMorePokemon(0, 53);
 
     _scrollController.addListener(() {
-      if (_scrollController.position.pixels ==
-          _scrollController.position.maxScrollExtent) {
-        // Llegamos al final del scroll, carguemos más Pokémon
-        offset += limit;
-        _fetchPokemonList(offset, limit).then((newPokemonList) {
-          setState(() {
-            pokemonList.addAll(newPokemonList);
-          });
-        });
+      if (_scrollController.position.pixels >= _scrollController.position.maxScrollExtent - 200 &&
+          !_pokemonListProvider.isLoading) {
+        _pokemonListProvider.fetchMorePokemon(_pokemonListProvider.pokemonList.length, 53);
       }
     });
   }
 
-  Future<List<Map<String, dynamic>>> _fetchPokemonList(
-      int offset, int limit) async {
-    final response = await ApiService.fetchPokemonList(limit, offset);
-    return response;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
+    return ChangeNotifierProvider(
+      create: (_) => _pokemonListProvider,
+      child: Scaffold(
+        appBar: const MyAppBar(appBarText: "POKÉDEX"),
+        backgroundColor: darkPokedex,
+        body: Column(
+          children: [
+            const Toolbar(),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: Consumer<PokemonListProvider>(
+                  builder: (context, provider, _) {
+                    if (provider.isLoading && provider.pokemonList.isEmpty) {
+                      return Container(
+                        height: 100,
+                        width: 100,
+                        alignment: Alignment.center,
+                        child: const CircularProgressIndicator()
+                        );
+                    } else if (provider.pokemonList.isEmpty) {
+                      return const Text('No Pokemon available.');
+                    } else {
+                      return GridView.builder(
+                        controller: _scrollController,
+                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          mainAxisSpacing: 16.0,
+                        ),
+                        itemCount: provider.pokemonList.length,
+                        itemBuilder: (context, index) {
+                          final pokemonInfo = provider.pokemonList[index];
+                
+                          return GestureDetector(
+                            onTap: () {
+                              
+                            },
+                            child: PokemonCard(
+                              pokemonInfo: pokemonInfo,
+                            ),
+                          );
+                        },
+                      );
+                    }
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
