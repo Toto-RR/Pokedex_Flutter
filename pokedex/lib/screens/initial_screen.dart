e29aa069a66e7070186debb2c14405caf46b6faa
Toto-RR <antonio.romanos@estudiantat.upc.edu>
2024-01-10 01:55:24 +0100

    initial screen started rework

--- pokedex/lib/screens/initial_screen.dart
+++ pokedex/lib/screens/initial_screen.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex/api/poke_api.dart';
-import 'package:pokedex/constants.dart';
-import 'package:pokedex/widget/my_appbar.dart';
-import 'package:pokedex/widget/poke_card.dart';
-import 'package:pokedex/widget/toolbar.dart';
-
 class InitialScreen extends StatefulWidget {
   const InitialScreen({Key? key}) : super(key: key);
 
@@ -50,48 +45,7 @@ class _InitialScreenState extends State<InitialScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const MyAppBar(appBarText: "POKÉDEX"),
-      backgroundColor: darkPokedex,
-      body: Column(
-        children: [
-          Toolbar(),
-          Expanded(
-            child: FutureBuilder<List<Map<String, dynamic>>>(
-              future: futurePokemonList,
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return CircularProgressIndicator();
-                } else if (snapshot.hasError) {
-                  return Text('Error: ${snapshot.error}');
-                } else {
-                  pokemonList = snapshot.data!;
-                  return GridView.builder(
-                    controller: _scrollController,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      mainAxisSpacing: 16.0,
-                    ),
-                    itemCount: pokemonList.length,
-                    itemBuilder: (context, index) {
-                      final pokemon = pokemonList[index];
-                      final name = pokemon['name'];
-                      final types = pokemon['types'];
-                      final id = pokemon['id'].toString().padLeft(3, '0');
-
-                      return PokemonCard(
-                        id: id,
-                        name: name,
-                        types: types,
-                      );
-                    },
-                  );
-                }
-              },
-            ),
-          ),
-        ],
-      ),
+      
     );
   }
 
