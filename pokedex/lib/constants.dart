718609a6d72ec9436ef60d7cc237454d89d7ba59
Toto-RR <antonio.romanos@estudiantat.upc.edu>
2024-01-10 21:22:32 +0100

    gamecolors added

--- pokedex/lib/constants.dart
+++ pokedex/lib/constants.dart
@@ -5,10 +5,43 @@ const Color redPokedex = Color(0xFFcc322a);
 const Color yellowPokedex = Color(0xFFe6d596);
 const Color darkRedPokedex = Color(0xFF642c34);
 const Color oldRedPokedex = Color(0xFFb45c64);
-const Color bluePokedex = Color(0xFFa7b9d3);
+const Color bluePokedex = Color.fromARGB(255, 70, 118, 185);
 const Color brownPokedex = Color(0xFF4c3434);
 const Color darkPokedex = Color.fromARGB(255, 29, 29, 29);
 const Color greyBackground = Color.fromARGB(255, 85, 85, 85);
+
+const Color pokemonRed = Color(0xFFe12d23);
+const Color pokemonBlue = Color(0xFF033c5b);
+const Color pokemonYellow = Color(0xFFd7ad01);
+const Color pokemonGold = Color(0xFFa89a69);
+const Color pokemonSilver = Color(0xFFa2aab1);
+const Color pokemonCrystal = Color(0xFF6bb694);
+const Color pokemonRuby = Color(0xFFba3728);
+const Color pokemonSapphire = Color(0xFF0f608e);
+const Color pokemonFireRed = Color(0xFFd75e37);
+const Color pokemonLeafGreen = Color(0xFF96c74c);
+const Color pokemonEmmerald = Color(0xFF0f9a45);
+const Color pokemonDiamond = Color(0xFF576f84);
+const Color pokemonPearl = Color(0xFFd28f87);
+const Color pokemonPlatinum = Color(0xFF9d9fad);
+const Color pokemonHeartGold = Color(0xFFd4ac55);
+const Color pokemonSoulSilver = Color(0xFFcfd5d5);
+const Color pokemonBlack = Color(0xFF010706);
+const Color pokemonWhite = Color(0xFFffffff);
+const Color pokemonBlack2 = Color(0xFF3f91b9);
+const Color pokemonWhite2 = Color(0xFFca2704);
+const Color pokemonX = Color(0xFF214c71);
+const Color pokemonY = Color(0xFFc12836);
+const Color pokemonOmegaRuby = Color(0xFFce2d43);
+const Color pokemonAlphaSapphire = Color(0xFF3a979a);
+const Color pokemonSun = Color(0xFFf9dfa1);
+const Color pokemonMoon = Color(0xFF79c9ed);
+const Color pokemonUltraSun = Color(0xFFf9dfa1);
+const Color pokemonUltraMoon = Color(0xFF79c9ed);
+const Color pokemonSword = Color(0xFF14527b);
+const Color pokemonShield = Color(0xFF99120f);
+const Color pokemonViolet = Color(0xFF722372);
+const Color pokemonScarlet = Color(0xFFb61928);
 
 
 // TEXT STYLES -------------------------------
@@ -25,6 +58,18 @@ TextStyle tabTitle = const TextStyle(
 TextStyle whiteTitleStyle = const TextStyle(
   color: Colors.white,
   fontSize: 40,
+  shadows: [
+    BoxShadow(
+      color: Colors.black,
+      blurRadius: 3,
+      offset: Offset(0, 2),
+    ),
+  ],
+);
+
+TextStyle whiteSubTitleStyle = const TextStyle(
+  color: Colors.white,
+  fontSize: 30,
   shadows: [
     BoxShadow(
       color: Colors.black,
