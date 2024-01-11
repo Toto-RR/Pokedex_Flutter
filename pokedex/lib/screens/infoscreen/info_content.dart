e06cc38ea7e77590a130cda9f90e323f90b83608
Víctor Gil Espallargas <victorgile14@gmail.com>
2024-01-11 16:38:21 +0100

    info_content part2

--- pokedex/lib/screens/infoscreen/info_content.dart
+++ pokedex/lib/screens/infoscreen/info_content.dart
@@ -43,3 +43,75 @@ class InfoGeneration extends StatelessWidget {
   }) : super(key: key);
 
   final GenerationInfo generation;
+
+ @override
+  Widget build(BuildContext context) {
+    Color beginColor = Colors.grey;
+    Color endColor = Colors.grey;
+    Color fontColor = Colors.white;
+
+    switch (generation.generation) {
+      case 'generation-i':
+        beginColor = pokemonRed;
+        endColor = pokemonBlue;
+        fontColor = Colors.white;
+        break;
+      case 'generation-ii':
+        beginColor = pokemonGold;
+        endColor = pokemonSilver;
+        fontColor = Colors.white;
+        break;
+      case 'generation-iii':
+        beginColor = pokemonRuby;
+        endColor = pokemonSapphire;
+        fontColor = Colors.white;
+        break;
+      case 'generation-iv':
+        beginColor = pokemonDiamond;
+        endColor = pokemonPearl;
+        fontColor = Colors.white;
+        break;
+      case 'generation-v':
+        beginColor = pokemonBlack;
+        endColor = pokemonWhite;
+        fontColor = Colors.white;
+        break;
+      case 'generation-vi':
+        beginColor = pokemonX;
+        endColor = pokemonY;
+        fontColor = Colors.white;
+        break;
+      case 'generation-vii':
+        beginColor = pokemonSun;
+        endColor = pokemonMoon;
+        fontColor = Colors.white;
+        break;
+      case 'generation-viii':
+        beginColor = pokemonSword;
+        endColor = pokemonShield;
+        fontColor = Colors.white;
+        break;
+      case 'generation-ix':
+        beginColor = pokemonViolet;
+        endColor = pokemonScarlet;
+        fontColor = Colors.white;
+        break;
+      default:
+        beginColor = Colors.grey;
+        endColor = Colors.grey;
+        break;
+    }
+
+TextStyle generationPillStyle = TextStyle(
+      color: fontColor,
+      fontSize: 15,
+      shadows: const [
+        BoxShadow(
+          color: Colors.black,
+          offset: Offset(2, 1),
+          blurRadius: 2,
+          blurStyle: BlurStyle.solid,
+          spreadRadius: BorderSide.strokeAlignOutside
+        )
+      ],
+    );
